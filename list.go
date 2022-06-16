2db70916265f56afbec4dba0eac4171e95e407b3
snowmerak <snowmerak@outlook.com>
2022-06-17 02:22:01 +0900

    add buffered list code

--- list.go
+++ list.go
@@ -5,15 +5,43 @@ import (
 	"sync"
 )
 
+var maxDataLen = os.Getpagesize()
+
 type node struct {
-	data []byte
-	next *node
+	data  []byte
+	start int
+	end   int
+	next  *node
+}
+
+func (n *node) append(data []byte) int {
+	appendingLength := maxDataLen - n.end
+	if appendingLength > len(data) {
+		appendingLength = len(data)
+	}
+	for i := n.end; i < n.end+appendingLength; i++ {
+		n.data[i] = data[i-n.end]
+	}
+	n.end += appendingLength
+	return appendingLength
+}
+
+func (n *node) read(data []byte) (int, bool) {
+	readingLen := n.end - n.start
+	if readingLen > len(data) {
+		readingLen = len(data)
+	}
+	for i := n.start; i < n.start+readingLen; i++ {
+		data[i-n.start] = n.data[i]
+	}
+	n.start += readingLen
+	return readingLen, n.start == n.end
 }
 
 var nodePool = sync.Pool{
 	New: func() any {
 		nb := node{
-			data: make([]byte, os.Getpagesize()),
+			data: make([]byte, maxDataLen),
 			next: nil,
 		}
 		return &nb
@@ -25,6 +53,12 @@ func newNode() *node {
 }
 
 func popNode(n *node) {
+	n.start = 0
+	n.end = 0
+	for i := 0; i < len(n.data); i++ {
+		n.data[i] = 0
+	}
+	n.next = nil
 	nodePool.Put(n)
 }
 
@@ -35,4 +69,40 @@ type BufferedList struct {
 
 func New() BufferedList {
 	return BufferedList{}
+}
+
+func (b *BufferedList) Append(data []byte) {
+	if b.head == nil {
+		b.head = newNode()
+		b.tail = b.head
+	}
+	appendedLen := b.tail.append(data)
+	for appendedLen < len(data) {
+		b.tail.next = newNode()
+		b.tail = b.tail.next
+		appendedLen += b.tail.append(data[appendedLen:])
+	}
+}
+
+func (b *BufferedList) Read(data []byte) int {
+	if b.head == nil {
+		return 0
+	}
+	index := 0
+	for {
+		readLen, isEmpty := b.head.read(data[index:])
+		index += readLen
+		if isEmpty {
+			popNode(b.head)
+			b.head = b.head.next
+			if b.head == nil {
+				b.tail = nil
+				break
+			}
+		}
+		if index == len(data) {
+			break
+		}
+	}
+	return index
 }
